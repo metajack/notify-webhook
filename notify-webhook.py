b572cc5f9ba5a5195f2d6b34d33c792cfe8cf382
Muness Alrubaie <muness@thinkrelevance.com>
2009-01-17 03:18:26 +0800

    Git 1.6 change
    Signed-off-by: Jack Moffitt <metajack@gmail.com>

--- notify-webhook.py
+++ notify-webhook.py
@@ -21,7 +21,7 @@ EMAIL_RE = re.compile("^(.*) <(.*)>$")
 
 
 def get_revisions(old, new):
-    git = subprocess.Popen(['git-rev-list', '--pretty=medium', '%s..%s' % (old, new)], stdout=subprocess.PIPE)
+    git = subprocess.Popen(['git', 'rev-list', '--pretty=medium', '%s..%s' % (old, new)], stdout=subprocess.PIPE)
     sections = git.stdout.read().split('\n\n')[:-1]
 
     revisions = []
