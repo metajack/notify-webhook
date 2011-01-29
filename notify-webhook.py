9cee9b91b695daa9426284768e03db87aea2547e
Andrew Hampe <andrew.hampe@gmail.com>
2011-01-28 19:34:16 -0600

    Converted tabs to spaces.

--- notify-webhook.py
+++ notify-webhook.py
@@ -23,70 +23,70 @@ EMAIL_RE = re.compile("^(.*) <(.*)>$")
 def get_revisions(old, new):
     git = subprocess.Popen(['git', 'rev-list', '--pretty=medium', '%s..%s' % (old, new)], stdout=subprocess.PIPE)
     sections = git.stdout.read().split('\n\n')[:-1]
-
+    
     revisions = []
     s = 0
     while s < len(sections):
-	lines = sections[s].split('\n')
-	    
-	# first line is 'commit HASH\n'
-	props = {'id': lines[0].strip().split(' ')[1]}
-	    
-	# read the header
-	for l in lines[1:]:
-	    key, val = l.split(' ', 1)
-	    props[key[:-1].lower()] = val.strip()
-
-	# read the commit message
-	props['message'] = sections[s+1]
-
-	# use github time format
-	basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
-	tzstr = props['date'][-5:]
-	props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
-
-	# split up author
-	m = EMAIL_RE.match(props['author'])
-	if m:
-	    props['name'] = m.group(1)
-	    props['email'] = m.group(2)
-	else:
-	    props['name'] = 'unknown'
-	    props['email'] = 'unknown'
-	del props['author']
-	
-	revisions.append(props)
-	s += 2
-	
+        lines = sections[s].split('\n')
+        
+        # first line is 'commit HASH\n'
+        props = {'id': lines[0].strip().split(' ')[1]}
+        
+        # read the header
+        for l in lines[1:]:
+            key, val = l.split(' ', 1)
+            props[key[:-1].lower()] = val.strip()
+        
+        # read the commit message
+        props['message'] = sections[s+1]
+        
+        # use github time format
+        basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
+        tzstr = props['date'][-5:]
+        props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+        
+        # split up author
+        m = EMAIL_RE.match(props['author'])
+        if m:
+            props['name'] = m.group(1)
+            props['email'] = m.group(2)
+        else:
+            props['name'] = 'unknown'
+            props['email'] = 'unknown'
+        del props['author']
+        
+        revisions.append(props)
+        s += 2
+    
     return revisions
 
 def make_json(old, new, ref):
     data = {
-	'before': old,
-	'after': new,
-	'ref': ref,
-	'repository': {
-	    'url': REPO_URL,
-	    'name': REPO_NAME,
-	    'description': REPO_DESC,
-	    'owner': {
-		'name': REPO_OWNER_NAME,
-		'email': REPO_OWNER_EMAIL
-		}
-	    }
-	}
-
+        'before': old,
+        'after': new,
+        'ref': ref,
+        'repository': {
+            'url': REPO_URL,
+            'name': REPO_NAME,
+            'description': REPO_DESC,
+            'owner': {
+                'name': REPO_OWNER_NAME,
+                'email': REPO_OWNER_EMAIL
+                }
+            }
+        }
+    
     revisions = get_revisions(old, new)
     commits = []
     for r in revisions:
-	commits.append({'id': r['id'],
-			'author': {'name': r['name'], 'email': r['email']},
-			'url': COMMIT_URL % r['id'],
-			'message': r['message'],
-			'timestamp': r['date']
-			})
+        commits.append({'id': r['id'],
+                        'author': {'name': r['name'], 'email': r['email']},
+                        'url': COMMIT_URL % r['id'],
+                        'message': r['message'],
+                        'timestamp': r['date']
+                        })
     data['commits'] = commits
-
+    
     return json.dumps(data)
 
 
@@ -94,7 +94,6 @@ def post(url, data):
     u = urllib2.urlopen(POST_URL, urllib.urlencode({'payload': data}))
     u.read()
     u.close()
-
 
 if __name__ == '__main__':
     for line in sys.stdin.xreadlines():
