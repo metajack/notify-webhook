05615b79daf8f55bfec6e78d19966161b741b4bc
Benjie Gillam <benjie@jemjie.com>
2012-05-21 12:33:46 +0100

    Read settings from git config/description.
    We also read the .git/description file.
    
    If details are not known (e.g. the setting is unset) then output null.
    
    Keys about this repo:
      meta.url
      meta.ownername
      meta.owneremail
    
    Key specifying where to POST to:
      hooks.webhookurl

--- notify-webhook.py
+++ notify-webhook.py
@@ -3,22 +3,46 @@
 import sys
 import urllib, urllib2
 import re
+import os
 import subprocess
 from datetime import datetime
 import simplejson as json
 
+def git(args):
+    args = ['git'] + args
+    git = subprocess.Popen(args, stdout = subprocess.PIPE)
+    details = git.stdout.read()
+    details = details.strip()
+    return details
 
-POST_URL = 'http://example.com'
-REPO_URL = 'http://example.com'
-COMMIT_URL = r'http://example.com/commit/%s'
-REPO_NAME = 'gitrepo'
-REPO_OWNER_NAME = 'Git U. Some'
-REPO_OWNER_EMAIL = 'git@example.com'
-REPO_DESC = ''
+def get_config(key):
+    details = git(['config', '%s' % (key)])
+    if len(details) > 0:
+        return details
+    else:
+        return None
 
+def get_repo_name():
+    if git(['rev-parse','--is-bare-repository']) == 'true':
+        return os.path.basename(os.getcwd())
+    else:
+        return os.path.basename(os.path.dirname(os.getcwd()))
+
+POST_URL = get_config('hooks.webhookurl')
+REPO_URL = get_config('meta.url')
+COMMIT_URL = get_config('meta.commiturl')
+if COMMIT_URL == None and REPO_URL != None:
+    COMMIT_URL = REPO_URL + r'/commit/%s'
+REPO_NAME = get_repo_name()
+REPO_DESC = get_config('meta.description') or open('description', 'r').read()
+REPO_OWNER_NAME = get_config('meta.ownername')
+REPO_OWNER_EMAIL = get_config('meta.owneremail')
+if REPO_OWNER_NAME is None:
+    REPO_OWNER_NAME = git(['log','--reverse','--format=%an']).split("\n")[0]
+if REPO_OWNER_EMAIL is None:
+    REPO_OWNER_EMAIL = git(['log','--reverse','--format=%ae']).split("\n")[0]
 
 EMAIL_RE = re.compile("^(.*) <(.*)>$")
-
 
 def get_revisions(old, new):
     git = subprocess.Popen(['git', 'rev-list', '--pretty=medium', '%s..%s' % (old, new)], stdout=subprocess.PIPE)
@@ -27,64 +51,67 @@ def get_revisions(old, new):
     revisions = []
     s = 0
     while s < len(sections):
-	lines = sections[s].split('\n')
-	    
-	# first line is 'commit HASH\n'
-	props = {'id': lines[0].strip().split(' ')[1]}
-	    
-	# read the header
-	for l in lines[1:]:
-	    key, val = l.split(' ', 1)
-	    props[key[:-1].lower()] = val.strip()
+        lines = sections[s].split('\n')
 
-	# read the commit message
-	props['message'] = sections[s+1]
+        # first line is 'commit HASH\n'
+        props = {'id': lines[0].strip().split(' ')[1]}
 
-	# use github time format
-	basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
-	tzstr = props['date'][-5:]
-	props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+        # read the header
+        for l in lines[1:]:
+            key, val = l.split(' ', 1)
+            props[key[:-1].lower()] = val.strip()
 
-	# split up author
-	m = EMAIL_RE.match(props['author'])
-	if m:
-	    props['name'] = m.group(1)
-	    props['email'] = m.group(2)
-	else:
-	    props['name'] = 'unknown'
-	    props['email'] = 'unknown'
-	del props['author']
-	
-	revisions.append(props)
-	s += 2
-	
+        # read the commit message
+        props['message'] = sections[s+1]
+
+        # use github time format
+        basetime = datetime.strptime(props['date'][:-6], "%a %b %d %H:%M:%S %Y")
+        tzstr = props['date'][-5:]
+        props['date'] = basetime.strftime('%Y-%m-%dT%H:%M:%S') + tzstr
+
+        # split up author
+        m = EMAIL_RE.match(props['author'])
+        if m:
+            props['name'] = m.group(1)
+            props['email'] = m.group(2)
+        else:
+            props['name'] = 'unknown'
+            props['email'] = 'unknown'
+        del props['author']
+
+        revisions.append(props)
+        s += 2
+
     return revisions
 
 def make_json(old, new, ref):
     data = {
-	'before': old,
-	'after': new,
-	'ref': ref,
-	'repository': {
-	    'url': REPO_URL,
-	    'name': REPO_NAME,
-	    'description': REPO_DESC,
-	    'owner': {
-		'name': REPO_OWNER_NAME,
-		'email': REPO_OWNER_EMAIL
-		}
-	    }
-	}
+        'before': old,
+        'after': new,
+        'ref': ref,
+        'repository': {
+            'url': REPO_URL,
+            'name': REPO_NAME,
+            'description': REPO_DESC,
+            'owner': {
+                'name': REPO_OWNER_NAME,
+                'email': REPO_OWNER_EMAIL
+                }
+            }
+        }
 
     revisions = get_revisions(old, new)
     commits = []
     for r in revisions:
-	commits.append({'id': r['id'],
-			'author': {'name': r['name'], 'email': r['email']},
-			'url': COMMIT_URL % r['id'],
-			'message': r['message'],
-			'timestamp': r['date']
-			})
+        url = None
+        if COMMIT_URL != None:
+            url = COMMIT_URL % r['id']
+        commits.append({'id': r['id'],
+                        'author': {'name': r['name'], 'email': r['email']},
+                        'url': url,
+                        'message': r['message'],
+                        'timestamp': r['date']
+                        })
     data['commits'] = commits
 
     return json.dumps(data)
@@ -96,8 +123,12 @@ def post(url, data):
     u.close()
 
 
+
 if __name__ == '__main__':
     for line in sys.stdin.xreadlines():
         old, new, ref = line.strip().split(' ')
         data = make_json(old, new, ref)
-        post(POST_URL, data)
+        if POST_URL:
+            post(POST_URL, data)
+        else:
+            print(data)
