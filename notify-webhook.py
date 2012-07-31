997428721be5b6f1ba688093375db7036fa2a695
Vojta Grec <vojta@easy.cz>
2012-07-31 12:53:54 +0200

    Applied POST error handling
    Taken from https://github.com/nikolaik/notify-webhook

--- notify-webhook.py
+++ notify-webhook.py
@@ -123,13 +123,13 @@ def make_json(old, new, ref):
 
     return json.dumps(data)
 
-
 def post(url, data):
-    u = urllib2.urlopen(POST_URL, urllib.urlencode({'payload': data}))
-    u.read()
-    u.close()
-
-
+    try:
+        u = urllib2.urlopen(POST_URL, urllib.urlencode({'payload': data}))
+        u.read()
+        u.close()
+    except urllib2.HTTPError as error:
+        print "POST to " + POST_URL + " returned error code " + str(error.code) + "."
 
 if __name__ == '__main__':
     for line in sys.stdin.xreadlines():
